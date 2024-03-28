37598b595743a9455ec9f059b7bcb34a95941ae4
Denis <126075349+Holizer@users.noreply.github.com>
2024-03-29 02:01:08 +0300

    Blya davai! Ahh ti suka

---
+++ PsuLive/PsuLive/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PsuLive.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
